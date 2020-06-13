e48c7ec043181cdaddd64227dcbd253d7a596e0a
Shahanur Islam Shagor <65355847+Shahanur1231@users.noreply.github.com>
2020-06-13 23:21:29 +0600

    first commit

---
+++ NewsTimeBangla/includes/footer.php
@@ -0,0 +1,7 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">শাহনুর ইসলাম সাগর</br>উত্তর বেগুনবাড়ি,তেঁজগাও শিল্পাঞ্চল এলাকা, ঢাকা- ১২০৮ </br>
+                ফোনঃ +৮৮ ০১৮ ৮৫৭৩ ৮৯২৯, +৮৮ ০১৭ ৯৬১৮ ৬৭৪৩</p>
+      </div>
+      <!-- /.container -->
+    </footer>
